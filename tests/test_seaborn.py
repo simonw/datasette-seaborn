1faf3f7cd85058a930fbd6dfacfd09de96cdf863
Simon Willison <swillison@gmail.com>
2020-09-11 09:27:12 -0700

    Failing test for #1

--- tests/test_seaborn.py
+++ tests/test_seaborn.py
@@ -2,6 +2,7 @@ from datasette.app import Datasette
 import httpx
 import pathlib
 import pytest
+import struct
 import sqlite3
 
 
@@ -16,15 +17,39 @@ def ds(tmp_path_factory):
 
 
 @pytest.mark.asyncio
-async def test_basic_plot(ds):
+async def test_requires_seaborn(ds):
     app = ds.app()
     async with httpx.AsyncClient(app=app) as client:
         response = await client.get("http://localhost/penguins/penguins.seaborn")
         assert response.status_code == 500
         assert response.text == "_seaborn= is required"
-        # Now try with _seaborn=relplot
-        response = await client.get(
-            "http://localhost/penguins/penguins.seaborn?_seaborn=relplot"
-        )
-        assert response.status_code == 200
-        assert response.headers["content-type"] == "image/png"
+
+
+@pytest.mark.asyncio
+async def test_image_dimensions_do_not_leak(ds):
+    # https://github.com/simonw/datasette-seaborn/issues/1#issuecomment-691187197
+    app = ds.app()
+
+    async def get_dims(path):
+        async with httpx.AsyncClient(app=app) as client:
+            response = await client.get("http://localhost{}".format(path))
+            assert response.status_code == 200
+            assert response.headers["content-type"] == "image/png"
+            return png_dims(response.content)
+
+    dims1 = await get_dims("/penguins/penguins.seaborn?_seaborn=histplot")
+    assert dims1 == (640, 480)
+
+    dims2 = await get_dims("/penguins/penguins.seaborn?_seaborn=relplot")
+    assert dims2 == (650, 484)
+
+    dims3 = await get_dims("/penguins/penguins.seaborn?_seaborn=histplot")
+    assert dims3 == (640, 480)
+
+    dims4 = await get_dims("/penguins/penguins.seaborn?_seaborn=relplot")
+    assert dims4 == (650, 484)
+
+
+def png_dims(png_bytes):
+    w, h = struct.unpack(">LL", png_bytes[16:24])
+    return int(w), int(h)
